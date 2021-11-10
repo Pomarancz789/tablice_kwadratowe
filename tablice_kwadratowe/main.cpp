077c16b064431887f11cbb350f1d7702d49e25aa
Pomarancz789 <granat-02krawcy@icloud.com>
2021-11-10 10:22:35 +0100

    Initial Commit

---
+++ tablice_kwadratowe/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tablice_kwadratowe
+//
+//  Created by Jakub Wtorkiewicz on 10/11/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
