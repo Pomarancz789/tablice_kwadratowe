7cb7e5c22fb263ab5fa8a021e4bcb3840c58c804
Pomarancz789 <granat-02krawcy@icloud.com>
2021-11-10 17:24:55 +0100

    działą?

--- tablice_kwadratowe/main.cpp
+++ tablice_kwadratowe/main.cpp
@@ -1,14 +1,43 @@
-//
-//  main.cpp
-//  tablice_kwadratowe
-//
-//  Created by Jakub Wtorkiewicz on 10/11/2021.
-//
-
 #include <iostream>
+#include<cstdio>
+#include<cstdlib>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    int tab[5][5]={};
+    
+    srand(time(NULL));
+    
+    //wypełnianie tablicy możliwymi rzutami kostki (liczby 1-6)
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<5; j++) {
+            tab[i][j]=1+rand()%(6-1+1);
+            cout << tab[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //wypełnianie tablicy możliwymi rzutami kostki (liczby 1-6)
+    
+    
+    //sprawdzanie ile jest szóstek na przekątnej
+    for(int h=0; h<6; h++){
+        if (tab[h][h]==6) {
+            cout << "Szóstka detected na indexach: [" <<h << "][" << h << "]" << endl;
+        }
+    }
+    //sprawdzanie ile jest szóstek na przekątnej
+    
+    
+    //suma elementów nad przekątną
+    int suma=0;
+    for (int k=0; k<5; k++) {
+        for (int l=k+1; l<5; l++) {
+            suma+=tab[k][l];
+        }
+    }
+    cout<<"suma elementow nad przekatna: " << suma<<endl;
+    //suma elementów nad przekątną
+    
+    
     return 0;
 }
